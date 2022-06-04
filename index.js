8a05e156f03bbe3c10336f617d1225f9937919f7
Eugene <eugeneayonga@gmail.com>
2022-06-04 23:10:49 +0300

    commit tree 371afc710280ed4e8d267fb29605d38f9682c2f1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+/* const element = document.getElementById("main");  
+element.remove();
+
+const newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+
+newHeader.textContent = "YOUR - NAME is the champion";*/
+
+
+/* document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion";*/
+
+const element = document.getElementById("main");  
+element.remove();
+
+let newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "YOUR - NAME is the champion";
